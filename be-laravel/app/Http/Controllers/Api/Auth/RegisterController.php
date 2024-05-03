63e7766cae5900fddc6bbaad3dbdd0ee7d6f7cc9
vunv <nguyenvanvu.bkhn@gmail.com>
2024-05-03 17:16:28 +0700

    init project

---
+++ be-laravel/app/Http/Controllers/Api/Auth/RegisterController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Api\Controllers\Controller as ApiBaseController;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends ApiBaseController
+{
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        $user = User::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'password' => Hash::make($validatedData['password']),
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return $this->success([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
+}
---
+++ be-laravel/app/Http/Controllers/Api/Controller.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Api\Controllers;
+
+use Jiannei\Response\Laravel\Support\Traits\JsonResponseTrait;
+
+abstract class Controller
+{
+    use JsonResponseTrait;
+}
